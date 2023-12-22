0c4d23fa9f08b5500d41d5686e27e536f8862b9d
Bea Fatima Dingson <dingson.176198@dasmarinas.sti.edu.ph>
2023-12-23 00:37:24 +0800

    changes

--- app/src/main/java/com/example/promee_createtask/MainActivity.java
+++ app/src/main/java/com/example/promee_createtask/MainActivity.java
@@ -1,53 +1,86 @@
 package com.example.promee_createtask;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    final Calendar myCalendar = Calendar.getInstance();
-    private EditText datePicker, timePicker;
+    private EditText datee;
+    private EditText startTimee;
+    private EditText endTimee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        datePicker = findViewById(R.id.date);
-        timePicker = findViewById(R.id.time);
-
-
-    }
-    private void selectDate(){
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        datee = findViewById(R.id.date);
+        startTimee = findViewById(R.id.startTime);
+        endTimee = findViewById(R.id.endTime);
+        datee.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, month);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
 
-                datePicker.setText(updateDate());
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        datee.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
+                    }
+                },  year, month, day);
+                datePickerDialog.show();
             }
-        };
-        datePicker.setOnClickListener(new View.OnClickListener(){
+        });
+        startTimee.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                new DatePickerDialog(MainActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        startTimee.setText(hourOfDay + ":" + minute);
+                    }
+                }, hour, minute, false);
+                timePickerDialog.show();
+            }
+        });
+        endTimee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        endTimee.setText(hourOfDay + ":" + minute);
+                    }
+                }, hour, minute, false);
+                timePickerDialog.show();
+
             }
         });
     }
-    private String updateDate(){
-        String myFormat = "yyyy-MM-dd";
-        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
-        return dateFormat.format(myCalendar.getTime());
-    }
 }
+
